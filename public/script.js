59a0a35557ea4bd3b2d5f20048af91cd5443edb2
José Gomes de Menezes <josegomesd6@gmail.com>
2023-11-03 12:08:20 -0300

    JavaScript almost complete

--- public/script.js
+++ public/script.js
@@ -1,1 +1,37 @@
-console.log('oi');
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+const Modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: modalBtnClose = document.querySelector('.modal button.close'),
+
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close() {
+    Modal.wrapper.classList.remove('open');
+  }
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+
+  const result = CalculateIMC(weight, height);
+  const message = `Seu IMC ideal é de ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open()
+
+}
+
+Modal.buttonClose.onclick = () => Modal.close()
+
+function CalculateIMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2);
+}
