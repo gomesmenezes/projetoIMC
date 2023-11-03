88b64df3144792fae741b9186b4ad2f7a1ca9029
José Gomes de Menezes <josegomesd6@gmail.com>
2023-11-03 12:41:30 -0300

    modularização

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,8 @@
+import { Modal } from './modal.js';
+
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
-
-const Modal = {
-
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: modalBtnClose = document.querySelector('.modal button.close'),
-
-  open() {
-    Modal.wrapper.classList.add('open')
-  },
-  close() {
-    Modal.wrapper.classList.remove('open');
-  }
-}
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -27,10 +15,9 @@ form.onsubmit = (event) => {
 
   Modal.message.innerText = message;
   Modal.open()
-
 }
 
-Modal.buttonClose.onclick = () => Modal.close()
+
 
 function CalculateIMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2);
